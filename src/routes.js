5c1fb5ddcd0d76d994ba00cbc59aacbaf4c3c93a
Tim Paine <t.paine154@gmail.com>
2021-06-26 14:19:35 -0400

    fix lint

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import Main from './components/Main';
 // import Signup from './components/Signup';
 import ScrollToTop from './components/ScrollTop';
 
-export default (props) => (
+export default () => (
   <HashRouter>
     <ScrollToTop>
       <Switch>
